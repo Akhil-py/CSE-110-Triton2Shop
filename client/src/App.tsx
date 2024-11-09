626828a4626727c4578a9d70e62d94c82fbaa341
dantemccflurry <dmccurty@ucsd.edu>
2024-11-09 14:34:32 -0800

    favorites list basic structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { AppProvider } from "./context/AppContext";
 import { HomePage } from "./views/HomePage";
 import React from 'react';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Components/Signup/signup';
 import Login from './Components/Login/login';
+import FavoritesList from "./Components/FavoritesList/FavoritesList";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/favorites" element={<FavoritesList/>} />
       </Routes>
     </div>
     </AppProvider>
