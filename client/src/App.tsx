a52f8321dfb1e87973f6d822ab34667e56e3cdc2
SV <vshree852@gmail.com>
2024-11-09 01:30:18 -0800

    initial product page design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { AppProvider } from "./context/AppContext";
 import { HomePage } from "./views/HomePage";
 import React from 'react';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Components/Signup/signup';
 import Login from './Components/Login/login';
+import ProductPage from "./Components/Product Page/product";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<ProductPage title={"Rotisserie Chicken"} price={12} imageUrl={""} description={"beeswax"} distance={12} sellerName={"triton"} sellerUsername={"tritonlowballer"} sellerContact={"123-456-7890"} />} />
       </Routes>
     </div>
     </AppProvider>
