bdd84c9e380bca2483390819a12d5ad7a676f1c9
datungLA <97591324+datungLA@users.noreply.github.com>
2024-11-08 18:15:19 -0800

    current

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import { AppProvider } from "./context/AppContext";
 import { HomePage } from "./views/HomePage";
 import { Route, Routes } from "react-router-dom";
-import Login from "./Components/Login/Login";
-import Signup from "./Components/Signup/Signup";
 const App = () => {
   // HINT: Wrap the MyBudgetTracker component with AppContextProvider
   return (<AppProvider>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
     </Routes>
   </AppProvider>);
 };
