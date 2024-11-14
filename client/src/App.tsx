5bd1b6098dc837e1d5a7a06acd768bb641e937c6
SV <vshree852@gmail.com>
2024-11-13 23:33:48 -0800

    initial UI design for request tracker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Components/Signup/signup';
 import Login from './Components/Login/login';
+import RequestTracker from './Components/Request Tracker/requestTracker';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/rq-tracker" element={<RequestTracker />} />
         </Routes>
       </div>
     </AppProvider>
